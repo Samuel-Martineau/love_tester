b3417f3df1362c1e3b74bcbfbbbacac8d7965e1b
Samuel-Martineau <samumartineau@gmail.com>
2020-06-17 14:49:34 -0400

    Prettier

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
-	const preloader = document.querySelector('#preloader');
-	const bg = document.querySelector("#page-bg");
-	const app = document.querySelector("#app");
-	setTimeout(() => {
-		preloader.classList.add('preloader-finish');
-		app.classList.remove("hidden");
-		bg.style.zIndex = "-999";
-	}, 500);
+  const preloader = document.querySelector('#preloader');
+  const bg = document.querySelector('#page-bg');
+  const app = document.querySelector('#app');
+  setTimeout(() => {
+    preloader.classList.add('preloader-finish');
+    app.classList.remove('hidden');
+    bg.style.zIndex = '-999';
+  }, 500);
 });
